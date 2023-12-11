101f4b4c1449e32e9fa3c503f0249bba2518bf7b
jn4624 <jn4624@gmail.com>
2023-12-11 14:25:33 +0900

    update todo 구현

--- src/main/java/com/exam/todo/service/TodoService.java
+++ src/main/java/com/exam/todo/service/TodoService.java
@@ -7,22 +7,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TodoService {
 
-    private final TodoRepository todoRepository;
+    private final TodoRepository repository;
 
     public List<TodoEntity> create(final TodoEntity entity) {
         validate(entity);
 
-        todoRepository.save(entity);
+        repository.save(entity);
 
         log.info("Entity Id : {} is saved", entity.getId());
 
-        return todoRepository.findByUserId(entity.getUserId());
+        return repository.findByUserId(entity.getUserId());
     }
 
     private void validate(final TodoEntity entity) {
@@ -38,7 +39,37 @@ public class TodoService {
     }
 
     public List<TodoEntity> retrieve(final String userId) {
-        return todoRepository.findByUserId(userId);
+        return repository.findByUserId(userId);
+    }
+
+    public List<TodoEntity> update(final TodoEntity entity) {
+        // (1) 저장할 엔티티가 유요한지 확인
+        validate(entity);
+
+        // (2) 넘겨받은 엔티티 id를 이용해 TodoEntity를 취득(존재하지 않는 엔티티는 업데이트할 수 없기 때문)
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+        original.ifPresent(todo -> {
+            // (3) 반환된 TodoEntity가 존재하면 값을 새 entity 값으로 변경
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            // (4) 데이터베이스에 새 값을 저장
+//            repository.save(todo);
+        });
+
+        // optional과 lambda가 익숙하지 않다면 아래 코드로 대체 가능
+        /*if (original.isPresent()) {
+            // (3) 반환된 TodoEntity가 존재하면 값을 새 entity 값으로 변경
+            final TodoEntity todo = original.get();
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            // (4) 데이터베이스에 새 값을 저장
+            repository.save(todo);
+        }*/
+
+        return retrieve(entity.getUserId());
     }
 
     public String testService() {
@@ -46,9 +77,9 @@ public class TodoService {
                 .title("My first todo item")
                 .build();
 
-        todoRepository.save(entity);
+        repository.save(entity);
 
-        TodoEntity savedEntity = todoRepository.findById(entity.getId()).get();
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
     }
 
